811479e45233a301b85685ca38b267485dac62e6
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-07-22 10:11:21 +0100

    BE-22 - Upgrade Hub to 2.0.314
    Updated the base version of Hub and metadata repository version.
    
    #BE-22 In Progress

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['java']['jdk_version'] = '7'
 
-default['cookbook_hub']['hub']['version'] = '2.0.156'
+default['cookbook_hub']['hub']['version'] = '2.0.314'
 default['cookbook_hub']['hub']['install_root_dir'] = '/opt/hub'
 default['cookbook_hub']['hub']['data_dir'] = '/root/.HubData'
 default['cookbook_hub']['hub']['backup_dir'] = '/root/.HubBackup'
-default['cookbook_hub']['hub']['download_url'] = 'https://download.jetbrains.com/hub/2.0/hub-ring-bundle-2.0.156.zip'
+default['cookbook_hub']['hub']['download_url'] = 'https://download.jetbrains.com/hub/2.0/hub-ring-bundle-2.0.314.zip'
 default['cookbook_hub']['hub']['memory_options'] = '-Xmx1g'
