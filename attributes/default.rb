a015d1ffa37e2807d55360d205f16f50fd968f31
Michael Wright <michaelw90@users.noreply.github.com>
2017-08-11 12:10:06 +0100

    Update Hub to 2017.3.6757

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['java']['jdk_version'] = '7'
 
-default['cookbook_hub']['hub']['version'] = '2017.1.4711'
+default['cookbook_hub']['hub']['version'] = '2017.3.6757'
 default['cookbook_hub']['hub']['install_root_dir'] = '/opt/hub'
 default['cookbook_hub']['hub']['data_dir'] = '/root/.HubData'
 default['cookbook_hub']['hub']['backup_dir'] = '/root/.HubBackup'
-default['cookbook_hub']['hub']['download_url'] = 'https://download.jetbrains.com/hub/2017.1/hub-ring-bundle-2017.1.4711.zip'
+default['cookbook_hub']['hub']['download_url'] = 'https://download.jetbrains.com/hub/2017.3/hub-ring-bundle-2017.3.6757.zip'
 default['cookbook_hub']['hub']['memory_options'] = '-Xmx1g'
